f614c31e55c681b998e184d37680078934dc6f45
James Cleveland <jamescleveland@gmail.com>
2012-02-11 01:38:31 +0000

    loads of stuff and twitter and all that

--- _wwwroot/js/script.js
+++ _wwwroot/js/script.js
@@ -1,8 +1,18 @@
-/* Author:
-
+/* Author: James Cleveland
 */
 
-
-
-
-
+$(function() {
+    $('.main-column').equalHeights();
+    $("#twitter").tweet({
+            username: "ntihero",
+            join_text: "auto",
+            avatar_size: 50,
+            count: 5,
+            auto_join_text_default: "we said,", 
+            auto_join_text_ed: "we",
+            auto_join_text_ing: "we were",
+            auto_join_text_reply: "we replied to",
+            auto_join_text_url: "we were checking out",
+            loading_text: "loading tweets..."
+        });
+});
