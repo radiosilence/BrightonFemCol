4f8305af3dea01bc696ecd6deec2c901406cc78a
James Cleveland <jamescleveland@gmail.com>
2012-02-11 02:48:07 +0000

    rearranging and tweaking to copy new yorker

--- _wwwroot/js/script.js
+++ _wwwroot/js/script.js
@@ -2,16 +2,23 @@
 */
 
 $(function() {
-    $('.main-column').equalHeights();
     $("#twitter").tweet({
-            username: "ntihero",
+            username: [
+                "littlespy",
+                'BrightonFemCol',
+                'ntihero',
+                'MrsEmBH',
+                'waywardtapper',
+                'squeamishbikini',
+                'frau_bh',
+            ],
             join_text: "auto",
             avatar_size: 50,
-            count: 5,
-            auto_join_text_default: "we said,", 
+            count: 10,
+            auto_join_text_default: "", 
             auto_join_text_ed: "we",
             auto_join_text_ing: "we were",
-            auto_join_text_reply: "we replied to",
+            auto_join_text_reply: "in reply to",
             auto_join_text_url: "we were checking out",
             loading_text: "loading tweets..."
         });
