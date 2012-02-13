6c5ed6c6772a51d4a733d3be3e9f0bbe4a6e709c
James Cleveland <jamescleveland@gmail.com>
2012-02-13 17:04:10 +0000

    making a test page

--- btnfemcol/static/js/script.js
+++ btnfemcol/static/js/script.js
@@ -4,13 +4,7 @@
 $(function() {
     $("#twitter").tweet({
             username: [
-                "littlespy",
                 'BrightonFemCol',
-                'ntihero',
-                'MrsEmBH',
-                'waywardtapper',
-                'squeamishbikini',
-                'frau_bh',
             ],
             join_text: "auto",
             avatar_size: 50,
@@ -19,7 +13,7 @@ $(function() {
             auto_join_text_ed: "we",
             auto_join_text_ing: "we were",
             auto_join_text_reply: "in reply to",
-            auto_join_text_url: "we were checking out",
+            auto_join_text_url: "",
             loading_text: "loading tweets..."
         });
 });
