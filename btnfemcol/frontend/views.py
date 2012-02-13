5e22e25864559a1a13c255e9cf6186ceacbeb159
James Cleveland <jamescleveland@gmail.com>
2012-02-13 23:33:36 +0000

    Page view stuff

--- btnfemcol/frontend/views.py
+++ btnfemcol/frontend/views.py
@@ -7,8 +7,24 @@ from flaskext.uploads import (UploadSet, configure_uploads, IMAGES,
 from btnfemcol.frontend import frontend
 from btnfemcol import uploaded_images, uploaded_avatars
 
-from btnfemcol.models import Article, User
+from btnfemcol.models import Article, User, Page
 
+
+
+@frontend.route('/article/<path:slug>')
+def show_article(slug):
+    page = Article.query.filter_by(slug=slug).first()
+    if not article:
+        abort(404)
+    return render_template('article.html', article=article)
+
+
+@frontend.route('/<path:slug>')
+def show_page(slug):
+    page = Page.query.filter_by(slug=slug).first()
+    if not page:
+        abort(404)
+    return render_template('page.html', page=page)
 
 @frontend.route('/')
 def home():
