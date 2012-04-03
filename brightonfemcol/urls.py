71db888214e18660362de898585073bec6d01e77
James Cleveland <jamescleveland@gmail.com>
2012-04-03 20:00:31 +0100

    django init

---
+++ brightonfemcol/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    (r'^api/v1/', include('fiber.api.urls')),
+    (r'^admin/fiber/', include('fiber.admin_urls')),
+    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('fiber',),}),
+    (r'', 'fiber.views.page'),
+)
