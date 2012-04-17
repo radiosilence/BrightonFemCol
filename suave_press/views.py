558a95e925bf462e51379d0b848885fbf128f9a9
James Cleveland <jamescleveland@gmail.com>
2012-04-17 10:09:11 +0100

    mptt related

--- suave_press/views.py
+++ suave_press/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.http import Http404
+from django.template.response import TemplateResponse
+
+from .models import Category, Article
+
+from .utils import get_category_from_slug, get_article_from_slug
+
+
+def view_category(request, category_slug=None):
+    category = get_category_from_slug(category_slug)
+
+    return TemplateResponse('suave_press/category.html', dict(
+        category=category
+    ))
+
+def view_article(request, category_slug=None, article_slug=None):
+    category = get_category_from_slug(category_slug)
+    article = get_article_from_slug(article_slug)
+
+    return TemplateResponse('suave_press/article.html', dict(
+        category=category,
+        article=article
+    ))
