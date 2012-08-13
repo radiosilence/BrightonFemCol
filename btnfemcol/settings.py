d879959064d299ee381da972e6ac204c337feab8
James Cleveland <jamescleveland@gmail.com>
2012-08-13 10:18:12 +0100

    pool recycle

--- btnfemcol/settings.py
+++ btnfemcol/settings.py
@@ -6,6 +6,7 @@ LOG_LOCATION = 'error.log'
 UPLOADED_AVATARS_DEST = 'btnfemcol/static/img/avatar'
 UPLOADED_IMAGES_DEST = 'btnfemcol/static/img/event'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+SQLALCHEMY_POOL_RECYCLE = 300
 
 # Get a key from http://code.google.com/apis/maps/signup.html
 GMAPS_KEY = ''
