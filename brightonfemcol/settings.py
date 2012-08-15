25de5f1024c04076a476d5398dd703897344a706
James Cleveland <jamescleveland@gmail.com>
2012-08-15 12:23:01 +0100

    Attachment icons.

--- brightonfemcol/settings.py
+++ brightonfemcol/settings.py
@@ -36,6 +36,8 @@ THUMBNAIL_FORMAT = 'PNG'
 
 DEFAULT_IMAGE = 'default-image.png'
 
+ICON_PATH = 'images/mimetypes/'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
