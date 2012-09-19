ab579a79b1afe6f392dbdcf021f60fe8bcf53f59
James Cleveland <jamescleveland@gmail.com>
2012-09-20 00:51:02 +0100

    Added migration dependencies.

--- suave_press/migrations/0008_auto__add_field_article_discussion_type.py
+++ suave_press/migrations/0008_auto__add_field_article_discussion_type.py
@@ -6,7 +6,9 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
-
+    depends_on = (
+        ('suave_discussion', '0001_initial'),
+    )
     def forwards(self, orm):
         # Adding field 'Article.discussion_type'
         db.add_column('suave_press_article', 'discussion_type',
