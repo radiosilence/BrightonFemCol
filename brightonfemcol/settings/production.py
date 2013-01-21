8f27c2fa2d92ae5ea477a7f070818fee0ea552ea
James Cleveland <jamescleveland@gmail.com>
2013-01-21 14:36:49 +0000

    Clearing up production config

--- brightonfemcol/settings/production.py
+++ brightonfemcol/settings/production.py
@@ -3,10 +3,8 @@ from .base import *
 ADMINS = (
     ('James Cleveland', 'jamescleveland@gmail.com'),
 )
-DEBUG = True
 DEBUG = False
 REQUIRE_DEBUG = DEBUG
 TEMPLATE_DEBUG = DEBUG
 
-SERVER_EMAIL = 'brightonfemcol@tealc'
-MANAGERS = ADMINS
+SERVER_EMAIL = 'server@brightonfeministcollective.org.uk'
